b932012cae688429b29966a828ff42fd93c70367
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-08-13 21:13:20 +0900

    分割代入のデフォルト値の設定

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 import type { User } from "../types/user";
 
 export const ListItem: FC<User> = (props) => {
-  const { id, name, age, personalColor } = props;
+  const { id, name, age, personalColor ="grey" } = props;
   return (
     <p style={{ color: personalColor }}>
       {id}:{name}({age})
