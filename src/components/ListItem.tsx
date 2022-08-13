3c7770080b21c9622461b1354239d72bc64de283
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-08-13 21:16:25 +0900

    defaultPropsによるデフォルト値の設定

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -2,10 +2,14 @@ import type { FC } from "react";
 import type { User } from "../types/user";
 
 export const ListItem: FC<User> = (props) => {
-  const { id, name, age, personalColor ="grey" } = props;
+  const { id, name, age, personalColor } = props;
   return (
     <p style={{ color: personalColor }}>
       {id}:{name}({age})
     </p>
   );
+};
+
+ListItem.defaultProps = {
+  personalColor: "grey"
 };
